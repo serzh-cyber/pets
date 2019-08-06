5dbda62d5ae05920be4a2e29dd87ea5151b45aaa
serzh-cyber <tsapman@gmail.com>
2019-08-06 10:36:14 +0600

    refactoring Feed - переименование в Portion, новое свойство amount, метод _construct, удаление метода getFeed

---
+++ src/Portion.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 22.07.2019
+ * Time: 9:34
+ */
+
+namespace App;
+
+class Portion
+{
+    protected $amount = 0;
+
+    /**
+     * Portion constructor.
+     * @param $amount
+     */
+    public function __construct($amount)
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
+}
