48190633b997f7d57e07047136aa32616a6b22b0
serzh-cyber <tsapman@gmail.com>
2019-08-06 10:15:50 +0600

    refactoring Application - использование методов eat и doToilet

--- src/Application.php
+++ src/Application.php
@@ -26,23 +26,28 @@ class Application
         $dogs       = PuppyFabric::create($parameters->getPuppyAmount());
         $cats       = KittyFabric::create($parameters->getKittyAmount());
         $animals    = array_merge($dogs, $cats);
-        shuffle($animals);
         $box        = BoxFabric::create($parameters->getBoxSquare());
         $outBox     = new OutBox();
+        $portions   = FoodFabric::create($parameters->getPuppyAmount()+$parameters->getKittyAmount());
         $inserter   = new Inserter();
-        $feed       = new Feed();
+
+        shuffle($animals);
+
+        foreach ($animals as $animal) {
+            $animal->eat(array_pop($portions));
+        }
 
         $inserter->insert($animals, $box, $outBox);
-        $box->feedPets($feed);
-        $outBox->feedPets($feed);
-        $box->toiletPets();
-        $outBox->toiletPets();
-        $box->feedPets($feed);
-        $outBox->feedPets($feed);
+        $box->doToilet();
+        $outBox->doToilet();
 
         $boxPresenter       = new BoxPresenter($box);
         $outBoxPresenter    = new OutBoxPresenter($outBox);
 
-        return $view->viewer($boxPresenter, $outBoxPresenter);
+        $view->viewer($boxPresenter, $outBoxPresenter);
+
+        if ($box->clearRequired()) {
+            $box->clearCrap();
+        }
     }
 }
